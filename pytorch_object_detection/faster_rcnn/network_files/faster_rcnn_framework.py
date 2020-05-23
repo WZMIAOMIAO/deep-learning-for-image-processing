86d1cd943f128f834d408cb9e74d6072b134c2d2
ww <180662@internet.com>
2020-05-23 08:28:49 +0800

    update readme

--- pytorch_object_detection/faster_rcnn/network_files/faster_rcnn_framework.py
+++ pytorch_object_detection/faster_rcnn/network_files/faster_rcnn_framework.py
@@ -306,7 +306,7 @@ class FasterRCNN(FasterRCNNBase):
         #  Multi-scale RoIAlign pooling
         if box_roi_pool is None:
             box_roi_pool = MultiScaleRoIAlign(
-                featmap_names=['0', '1', '2', '3'],  # 在哪些特征层进行预测
+                featmap_names=['0', '1', '2', '3'],  # 在哪些特征层进行roi pooling
                 output_size=[7, 7],
                 sampling_ratio=2)
 
--- pytorch_object_detection/faster_rcnn/train_mobilenet.py
+++ pytorch_object_detection/faster_rcnn/train_mobilenet.py
@@ -17,7 +17,7 @@ def create_model(num_classes):
     anchor_generator = AnchorsGenerator(sizes=((32, 64, 128, 256, 512),),
                                         aspect_ratios=((0.5, 1.0, 2.0),))
 
-    roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'],  # 在哪些特征层上进行下采样
+    roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'],  # 在哪些特征层上进行roi pooling
                                                     output_size=[7, 7],   # roi_pooling输出特征矩阵尺寸
                                                     sampling_ratio=2)  # 采样率
 
