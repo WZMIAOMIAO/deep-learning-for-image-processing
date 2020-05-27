38f165a0bfd9dd72519f82c507f5aac9d0e21916
wz <605169423@qq.com>
2020-05-27 23:23:36 +0800

    add confusionmatrix

---
+++ pytorch_classification/ConfusionMatrix/main.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms, datasets
+import json
+import os
+from model import MobileNetV2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class ConfusionMatrix(object):
+    """
+    注意，如果显示的图像不全，是matplotlib版本问题
+    本例程使用matplotlib-3.2.1绘制正常
+    """
+    def __init__(self, num_classes: int, labels: list):
+        self.matrix = np.zeros((num_classes, num_classes))
+        self.num_classes = num_classes
+        self.labels = labels
+
+    def update(self, preds, labels):
+        for p, t in zip(preds, labels):
+            self.matrix[t, p] += 1
+
+    def plot(self, normalize=False):
+        matrix = self.matrix
+        plot_title = 'Confusion matrix'
+        if normalize:
+            plot_title = 'Normalized Confusion matrix'
+            # 去除axis=1纬度，即求每行元素的和
+            matrix = matrix / matrix.sum(axis=1)[:, np.newaxis]
+        print(matrix)
+        plt.imshow(matrix, cmap=plt.cm.Blues)
+
+        # 设置x轴坐标label
+        plt.xticks(range(self.num_classes), self.labels, rotation=45)
+        # 设置y轴坐标label
+        plt.yticks(range(self.num_classes), self.labels)
+        # 显示colorbar
+        plt.colorbar()
+        plt.xlabel('Predicted Labels')
+        plt.ylabel('True Labels')
+        plt.title(plot_title)
+
+        # 在图中标注数量/概率信息
+        thresh = matrix.max() / 2
+        for x in range(self.num_classes):
+            for y in range(self.num_classes):
+                # 注意这里的matrix[y, x]不是matrix[x, y]
+                info = round(matrix[y, x], 2) if normalize else int(matrix[y, x])
+                plt.text(x, y, info,
+                         verticalalignment='center',
+                         horizontalalignment='center',
+                         color="white" if info > thresh else "black")
+        plt.tight_layout()
+        plt.show()
+
+
+if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    data_transform = transforms.Compose([transforms.Resize(256),
+                                         transforms.CenterCrop(224),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+    data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))  # get data root path
+    image_path = data_root + "/data_set/flower_data/"  # flower data set path
+
+    validate_dataset = datasets.ImageFolder(root=image_path + "val",
+                                            transform=data_transform)
+
+    batch_size = 16
+    validate_loader = torch.utils.data.DataLoader(validate_dataset,
+                                                  batch_size=batch_size, shuffle=False,
+                                                  num_workers=2)
+    net = MobileNetV2(num_classes=5)
+    # load pretrain weights
+    model_weight_path = "./MobileNetV2.pth"
+    net.load_state_dict(torch.load(model_weight_path))
+    net.to(device)
+
+    # read class_indict
+    try:
+        json_file = open('./class_indices.json', 'r')
+        class_indict = json.load(json_file)
+    except Exception as e:
+        print(e)
+        exit(-1)
+
+    labels = [label for _, label in class_indict.items()]
+    confusion = ConfusionMatrix(num_classes=5, labels=labels)
+    net.eval()
+    with torch.no_grad():
+        for val_data in validate_loader:
+            val_images, val_labels = val_data
+            outputs = net(val_images.to(device))
+            outputs = torch.softmax(outputs, dim=1)
+            outputs = torch.argmax(outputs, dim=1)
+            confusion.update(outputs.numpy(), val_labels.numpy())
+    confusion.plot(True)
