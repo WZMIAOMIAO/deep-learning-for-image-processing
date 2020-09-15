add mixed precision training ;fix bug Tring to pass too many CPU scalars to CUDA kernel

pytorch_object_detection/faster_rcnn/network_files/det_utils.py
@@ -82,8 +82,8 @@ class BalancedPositiveNegativeSampler(object):
                 matched_idxs_per_image, dtype=torch.uint8
             )
 
-            pos_idx_per_image_mask[pos_idx_per_image] = torch.tensor(1, dtype=torch.uint8)
-            neg_idx_per_image_mask[neg_idx_per_image] = torch.tensor(1, dtype=torch.uint8)
+            pos_idx_per_image_mask[pos_idx_per_image] = 1
+            neg_idx_per_image_mask[neg_idx_per_image] = 1
 
             pos_idx.append(pos_idx_per_image_mask)
             neg_idx.append(neg_idx_per_image_mask)
@@ -260,11 +260,11 @@ class BoxCoder(object):
         # xmin
         pred_boxes1 = pred_ctr_x - torch.tensor(0.5, dtype=pred_ctr_x.dtype, device=pred_w.device) * pred_w
         # ymin
-        pred_boxes2 = pred_ctr_y - torch.tensor(0.5, dtype=pred_ctr_x.dtype, device=pred_w.device) * pred_h
+        pred_boxes2 = pred_ctr_y - torch.tensor(0.5, dtype=pred_ctr_y.dtype, device=pred_h.device) * pred_h
         # xmax
         pred_boxes3 = pred_ctr_x + torch.tensor(0.5, dtype=pred_ctr_x.dtype, device=pred_w.device) * pred_w
         # ymax
-        pred_boxes4 = pred_ctr_y + torch.tensor(0.5, dtype=pred_ctr_x.dtype, device=pred_w.device) * pred_h
+        pred_boxes4 = pred_ctr_y + torch.tensor(0.5, dtype=pred_ctr_y.dtype, device=pred_h.device) * pred_h
         pred_boxes = torch.stack((pred_boxes1, pred_boxes2, pred_boxes3, pred_boxes4), dim=2).flatten(1)
         return pred_boxes
 
@@ -344,10 +344,10 @@ class Matcher(object):
             matched_vals < self.high_threshold
         )
         # iou小于low_threshold的matches索引置为-1
-        matches[below_low_threshold] = torch.tensor(self.BELOW_LOW_THRESHOLD)  # -1
+        matches[below_low_threshold] = self.BELOW_LOW_THRESHOLD  # -1
 
         # iou在[low_threshold, high_threshold]之间的matches索引置为-2
-        matches[between_thresholds] = torch.tensor(self.BETWEEN_THRESHOLDS)    # -2
+        matches[between_thresholds] = self.BETWEEN_THRESHOLDS    # -2
 
         if self.allow_low_quality_matches:
             assert all_matches is not None

pytorch_object_detection/faster_rcnn/network_files/faster_rcnn_framework.py
@@ -42,7 +42,7 @@ class FasterRCNNBase(nn.Module):
         return detections
 
     def forward(self, images, targets=None):
-        # type: (List[Tensor], Optional[List[Dict[str, Tensor]]])
+        # type: (List[Tensor], Optional[List[Dict[str, Tensor]]]) -> Tuple[Dict[str, Tensor], List[Dict[str, Tensor]]]
         """
         Arguments:
             images (list[Tensor]): images to be processed
@@ -79,6 +79,7 @@ class FasterRCNNBase(nn.Module):
         # original_image_sizes = [img.shape[-2:] for img in images]
 
         images, targets = self.transform(images, targets)  # 对图像进行预处理
+        # print(images.tensors.shape)
         features = self.backbone(images.tensors)  # 将图像输入backbone得到特征图
         if isinstance(features, torch.Tensor):  # 若只在一层特征层上预测，将feature放入有序字典中，并编号为‘0’
             features = OrderedDict([('0', features)])  # 若在多层特征层上预测，传入的就是一个有序字典
@@ -239,7 +240,7 @@ class FasterRCNN(FasterRCNNBase):
 
     def __init__(self, backbone, num_classes=None,
                  # transform parameter
-                 min_size=800, max_size=1344,      # 预处理resize时限制的最小尺寸与最大尺寸
+                 min_size=800, max_size=1000,      # 预处理resize时限制的最小尺寸与最大尺寸
                  image_mean=None, image_std=None,  # 预处理normalize时使用的均值和方差
                  # RPN parameters
                  rpn_anchor_generator=None, rpn_head=None,

pytorch_object_detection/faster_rcnn/train_utils/train_eval_utils.py
@@ -26,27 +26,30 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq,
 
         lr_scheduler = warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor)
 
+    enable_amp = True if "cuda" in device.type else False
     for images, targets in metric_logger.log_every(data_loader, print_freq, header):
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-        loss_dict = model(images, targets)
+        # 混合精度训练上下文管理器，如果在CPU环境中不起任何作用
+        with torch.cuda.amp.autocast(enabled=enable_amp):
+            loss_dict = model(images, targets)
 
-        losses = sum(loss for loss in loss_dict.values())
+            losses = sum(loss for loss in loss_dict.values())
 
-        # reduce losses over all GPUs for logging purpose
-        loss_dict_reduced = reduce_dict(loss_dict)
-        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+            # reduce losses over all GPUs for logging purpose
+            loss_dict_reduced = reduce_dict(loss_dict)
+            losses_reduced = sum(loss for loss in loss_dict_reduced.values())
 
-        loss_value = losses_reduced.item()
-        if isinstance(train_loss, list):
-            # 记录训练损失
-            train_loss.append(loss_value)
+            loss_value = losses_reduced.item()
+            if isinstance(train_loss, list):
+                # 记录训练损失
+                train_loss.append(loss_value)
 
-        if not math.isfinite(loss_value):  # 当计算的损失为无穷大时停止训练
-            print("Loss is {}, stopping training".format(loss_value))
-            print(loss_dict_reduced)
-            sys.exit(1)
+            if not math.isfinite(loss_value):  # 当计算的损失为无穷大时停止训练
+                print("Loss is {}, stopping training".format(loss_value))
+                print(loss_dict_reduced)
+                sys.exit(1)
 
         optimizer.zero_grad()
         losses.backward()