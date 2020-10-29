02c2bf03cb50ca84a348d328aff46d06f2d7e0e7
wz <WZMIAOMIAO@github.com>
2020-10-29 12:33:55 +0800

    add activation for dense

--- tensorflow_classification/Test5_resnet/predict.py
+++ tensorflow_classification/Test5_resnet/predict.py
@@ -37,7 +37,7 @@ feature.trainable = False
 model = tf.keras.Sequential([feature,
                              tf.keras.layers.GlobalAvgPool2D(),
                              tf.keras.layers.Dropout(rate=0.5),
-                             tf.keras.layers.Dense(1024),
+                             tf.keras.layers.Dense(1024, activation="relu"),
                              tf.keras.layers.Dropout(rate=0.5),
                              tf.keras.layers.Dense(5),
                              tf.keras.layers.Softmax()])
