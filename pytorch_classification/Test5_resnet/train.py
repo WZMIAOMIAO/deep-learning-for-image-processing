a2e1473fc31d4473ba6a0920bccd8dca745d9cbf
wz <605169423@qq.com>
2020-11-15 12:15:01 +0800

    update train resnet script

--- pytorch_classification/Test5_resnet/train.py
+++ pytorch_classification/Test5_resnet/train.py
@@ -21,7 +21,7 @@ def main():
                                    transforms.CenterCrop(224),
                                    transforms.ToTensor(),
                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
-    
+
     data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))  # get data root path
     image_path = data_root + "/data_set/flower_data/"  # flower data set path
 
