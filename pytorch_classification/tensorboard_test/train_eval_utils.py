a2d79e10f65c591061e8b83d9dea53185c28197c
wz <WZMIAOMIAO@github.com>
2021-01-04 18:33:20 +0800

    upload tensorboard test

---
+++ pytorch_classification/tensorboard_test/train_eval_utils.py
@@ -0,0 +1,62 @@
+import sys
+
+from tqdm import tqdm
+import torch
+
+
+def train_one_epoch(model, optimizer, data_loader, device, epoch):
+    model.train()
+    loss_function = torch.nn.CrossEntropyLoss()
+    mean_loss = torch.zeros(1).to(device)
+    optimizer.zero_grad()
+
+    data_loader = tqdm(data_loader)
+    for step, data in enumerate(data_loader):
+        images, labels = data
+        pred = model(images.to(device))
+
+        loss = loss_function(pred, labels.to(device))
+        loss.backward()
+        mean_loss = (mean_loss * step + loss.detach()) / (step + 1)  # update mean losses
+
+        # 打印平均loss
+        data_loader.desc = "[epoch {}] mean loss {}".format(epoch, round(mean_loss.item(), 3))
+
+        if not torch.isfinite(loss):
+            print('WARNING: non-finite loss, ending training ', loss)
+            sys.exit(1)
+
+        optimizer.step()
+        optimizer.zero_grad()
+
+    return mean_loss.item()
+
+
+@torch.no_grad()
+def evaluate(model, data_loader, device):
+    model.eval()
+
+    # 用于存储预测正确的样本个数
+    sum_num = torch.zeros(1).to(device)
+    # 统计验证集样本总数目
+    num_samples = len(data_loader.dataset)
+
+    # 打印验证进度
+    data_loader = tqdm(data_loader, desc="validation...")
+
+    for step, data in enumerate(data_loader):
+        images, labels = data
+        pred = model(images.to(device))
+        pred = torch.max(pred, dim=1)[1]
+        sum_num += torch.eq(pred, labels.to(device)).sum()
+
+    # 计算预测正确的比例
+    acc = sum_num.item() / num_samples
+
+    return acc
+
+
+
+
+
+
