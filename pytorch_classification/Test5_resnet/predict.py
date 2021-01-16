10acb6ccc2e51c0a2ea23ca3938a3e22049b4897
wz <WZMIAOMIAO@github.com>
2021-01-16 17:12:14 +0800

    update resnet code

--- pytorch_classification/Test5_resnet/predict.py
+++ pytorch_classification/Test5_resnet/predict.py
@@ -1,44 +1,63 @@
+import os
+import json
+
 import torch
-from model import resnet34
 from PIL import Image
 from torchvision import transforms
 import matplotlib.pyplot as plt
-import json
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+from model import resnet34
 
-data_transform = transforms.Compose(
-    [transforms.Resize(256),
-     transforms.CenterCrop(224),
-     transforms.ToTensor(),
-     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
-# load image
-img = Image.open("../tulip.jpg")
-plt.imshow(img)
-# [N, C, H, W]
-img = data_transform(img)
-# expand batch dimension
-img = torch.unsqueeze(img, dim=0)
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# read class_indict
-try:
-    json_file = open('./class_indices.json', 'r')
+    data_transform = transforms.Compose(
+        [transforms.Resize(256),
+         transforms.CenterCrop(224),
+         transforms.ToTensor(),
+         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+    # load image
+    img_path = "../tulip.jpg"
+    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    img = Image.open(img_path)
+    plt.imshow(img)
+    # [N, C, H, W]
+    img = data_transform(img)
+    # expand batch dimension
+    img = torch.unsqueeze(img, dim=0)
+
+    # read class_indict
+    json_path = './class_indices.json'
+    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+
+    json_file = open(json_path, "r")
     class_indict = json.load(json_file)
-except Exception as e:
-    print(e)
-    exit(-1)
 
-# create model
-model = resnet34(num_classes=5)
-# load model weights
-model_weight_path = "./resNet34.pth"
-model.load_state_dict(torch.load(model_weight_path, map_location=device))
-model.eval()
-with torch.no_grad():
-    # predict class
-    output = torch.squeeze(model(img))
-    predict = torch.softmax(output, dim=0)
-    predict_cla = torch.argmax(predict).numpy()
-print(class_indict[str(predict_cla)], predict[predict_cla].numpy())
-plt.show()
+    # create model
+    model = resnet34(num_classes=5).to(device)
+
+    # load model weights
+    weights_path = "./resNet34.pth"
+    assert os.path.exists(weights_path), "file: '{}' dose not exist.".format(weights_path)
+    model_weight_path = weights_path
+    model.load_state_dict(torch.load(model_weight_path, map_location=device))
+
+    # prediction
+    model.eval()
+    with torch.no_grad():
+        # predict class
+        output = torch.squeeze(model(img.to(device))).cpu()
+        predict = torch.softmax(output, dim=0)
+        predict_cla = torch.argmax(predict).numpy()
+
+    print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
+                                                 predict[predict_cla].numpy())
+    plt.title(print_res)
+    print(print_res)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
