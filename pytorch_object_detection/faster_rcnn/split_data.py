afd797501eee295784b34be7130564dada17b7e4
wz <180662@gree.com.cn>
2021-01-27 17:28:09 +0800

    update script

---
+++ pytorch_object_detection/faster_rcnn/split_data.py
@@ -0,0 +1,35 @@
+import os
+import random
+
+
+def main():
+    random.seed(0)  # 设置随机种子，保证随机结果可复现
+
+    files_path = "./VOCdevkit/VOC2012/Annotations"
+    assert os.path.exists(files_path), "path: '{}' does not exist.".format(files_path)
+
+    val_rate = 0.5
+
+    files_name = sorted([file.split(".")[0] for file in os.listdir(files_path)])
+    files_num = len(files_name)
+    val_index = random.sample(range(0, files_num), k=int(files_num*val_rate))
+    train_files = []
+    val_files = []
+    for index, file_name in enumerate(files_name):
+        if index in val_index:
+            val_files.append(file_name)
+        else:
+            train_files.append(file_name)
+
+    try:
+        train_f = open("train.txt", "x")
+        eval_f = open("val.txt", "x")
+        train_f.write("\n".join(train_files))
+        eval_f.write("\n".join(val_files))
+    except FileExistsError as e:
+        print(e)
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
