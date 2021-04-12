d94e349f879ee2d4f02812a951e138b0ac1c0cda
wz <180662@gree.com.cn>
2021-04-12 18:21:45 +0800

    update code

--- pytorch_object_detection/train_coco_dataset/train.py
+++ pytorch_object_detection/train_coco_dataset/train.py
@@ -37,16 +37,12 @@ def create_model(num_classes):
     return model
 
 
-def main():
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+def main(args):
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
     print("Using {} device training.".format(device.type))
 
     # 用来保存coco_info的文件
     results_file = "results{}.txt".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
-
-    # 检查保存权重文件夹是否存在，不存在则创建
-    if not os.path.exists("save_weights"):
-        os.makedirs("save_weights")
 
     data_transform = {
         "train": transforms.Compose([transforms.ToTensor(),
@@ -54,13 +50,13 @@ def main():
         "val": transforms.Compose([transforms.ToTensor()])
     }
 
-    COCO_root = "/data/coco2017"
+    COCO_root = args.data_path
 
     # load train data set
     # coco2017 -> annotations -> instances_train2017.json
     train_data_set = CocoDetection(COCO_root, "train", data_transform["train"])
     # 注意这里的collate_fn是自定义的，因为读取的数据包括image和targets，不能直接使用默认的方法合成batch
-    batch_size = 8
+    batch_size = args.batch_size
     nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
     print('Using %g dataloader workers' % nw)
     train_data_loader = torch.utils.data.DataLoader(train_data_set,
@@ -81,7 +77,7 @@ def main():
                                                       collate_fn=train_data_set.collate_fn)
 
     # create model num_classes equal background + 80 classes
-    model = create_model(num_classes=81)
+    model = create_model(num_classes=args.num_classes + 1)
     # print(model)
 
     model.to(device)
@@ -92,13 +88,26 @@ def main():
 
     # define optimizer
     params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.SGD(params, lr=0.001,
-                                momentum=0.9, weight_decay=1e-4)
+    optimizer = torch.optim.SGD(params, lr=args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
     # learning rate scheduler
-    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[16, 22], gamma=0.1)
+    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
+                                                        milestones=args.lr_steps,
+                                                        gamma=args.lr_gamma)
+    # 如果传入resume参数，即上次训练的权重地址，则接着上次的参数训练
+    if args.resume:
+        # If map_location is missing, torch.load will first load the module to CPU
+        # and then copy each parameter to where it was saved,
+        # which would result in all processes on the same machine using the same set of devices.
+        checkpoint = torch.load(args.resume, map_location=device)  # 读取之前保存的权重文件(包括优化器以及学习率策略)
+        model.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+        args.start_epoch = checkpoint['epoch'] + 1
 
-    num_epochs = 26
-    for epoch in range(num_epochs):
+    num_epochs = args.epochs
+    for epoch in range(args.start_epoch, args.epochs):
         # train for one epoch, printing every 50 iterations
         mean_loss, lr = utils.train_one_epoch(model, optimizer, train_data_loader,
                                               device, epoch, print_freq=50, warmup=True)
@@ -142,4 +151,51 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description=__doc__)
+
+    # 训练设备类型
+    parser.add_argument('--device', default='cuda:0', help='device')
+    # 训练数据集的根目录
+    parser.add_argument('--data-path', default='/data/coco2017', help='dataset')
+    # 检测目标类别数(不包含背景)
+    parser.add_argument('--num-classes', default=80, type=int, help='num_classes')
+    # 文件保存地址
+    parser.add_argument('--output-dir', default='./save_weights', help='path where to save')
+    # 若需要接着上次训练，则指定上次训练保存权重文件地址
+    parser.add_argument('--resume', default='', type=str, help='resume from checkpoint')
+    # 指定接着从哪个epoch数开始训练
+    parser.add_argument('--start_epoch', default=0, type=int, help='start epoch')
+    # 训练的总epoch数
+    parser.add_argument('--epochs', default=26, type=int, metavar='N',
+                        help='number of total epochs to run')
+    # 学习率
+    parser.add_argument('--lr', default=0.002, type=float,
+                        help='initial learning rate, 0.02 is the default value for training '
+                             'on 8 gpus and 2 images_per_gpu')
+    # SGD的momentum参数
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    # SGD的weight_decay参数
+    parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                        metavar='W', help='weight decay (default: 1e-4)',
+                        dest='weight_decay')
+    # 针对torch.optim.lr_scheduler.MultiStepLR的参数
+    parser.add_argument('--lr-steps', default=[16, 22], nargs='+', type=int,
+                        help='decrease lr every step-size epochs')
+    # 针对torch.optim.lr_scheduler.MultiStepLR的参数
+    parser.add_argument('--lr-gamma', default=0.1, type=float, help='decrease lr by a factor of lr-gamma')
+    # 训练的batch size
+    parser.add_argument('--batch_size', default=8, type=int, metavar='N',
+                        help='batch size when training.')
+
+    args = parser.parse_args()
+    print(args)
+
+    # 检查保存权重文件夹是否存在，不存在则创建
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    main(args)
