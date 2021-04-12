5d83728a9a75ec24a49cdd78d102cdd445f1af1b
wz <180662@gree.com.cn>
2021-04-12 14:18:12 +0800

    add training coco2017 project

---
+++ pytorch_object_detection/train_coco_dataset/backbone/vgg_model.py
@@ -0,0 +1,72 @@
+import torch.nn as nn
+import torch
+
+
+class VGG(nn.Module):
+    def __init__(self, features, class_num=1000, init_weights=False, weights_path=None):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Linear(512*7*7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, class_num)
+        )
+        if init_weights and weights_path is None:
+            self._initialize_weights()
+
+        if weights_path is not None:
+            self.load_state_dict(torch.load(weights_path))
+
+    def forward(self, x):
+        # N x 3 x 224 x 224
+        x = self.features(x)
+        # N x 512 x 7 x 7
+        x = torch.flatten(x, start_dim=1)
+        # N x 512*7*7
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                # nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                # nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+def make_features(cfg: list):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == "M":
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            layers += [conv2d, nn.ReLU(True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+cfgs = {
+    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+def vgg(model_name="vgg16", weights_path=None):
+    assert model_name in cfgs, "Warning: model number {} not in cfgs dict!".format(model_name)
+    cfg = cfgs[model_name]
+
+    model = VGG(make_features(cfg), weights_path=weights_path)
+    return model
---
+++ pytorch_object_detection/train_coco_dataset/train.py
@@ -0,0 +1,141 @@
+import os
+import datetime
+
+import torch
+import torchvision
+
+import transforms
+from network_files import FasterRCNN, AnchorsGenerator
+from backbone import MobileNetV2, vgg
+from my_dataset import CocoDetection
+from train_utils import train_eval_utils as utils
+
+
+def create_model(num_classes):
+    # https://download.pytorch.org/models/vgg16-397923af.pth
+    # 如果使用vgg16的话就下载对应预训练权重并取消下面两行注释，接着把mobilenetv2模型对应的两行代码注释掉
+    vgg_feature = vgg(model_name="vgg16", weights_path="./backbone/vgg16.pth").features
+    backbone = torch.nn.Sequential(*list(vgg_feature._modules.values())[:-1])  # 删除feature中最后的maxpool层
+    backbone.out_channels = 512
+
+    anchor_generator = AnchorsGenerator(sizes=((32, 64, 128, 256, 512),),
+                                        aspect_ratios=((0.5, 1.0, 2.0),))
+
+    roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'],  # 在哪些特征层上进行roi pooling
+                                                    output_size=[7, 7],   # roi_pooling输出特征矩阵尺寸
+                                                    sampling_ratio=2)  # 采样率
+
+    model = FasterRCNN(backbone=backbone,
+                       num_classes=num_classes,
+                       rpn_anchor_generator=anchor_generator,
+                       box_roi_pool=roi_pooler)
+
+    return model
+
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("Using {} device training.".format(device.type))
+
+    # 用来保存coco_info的文件
+    results_file = "results{}.txt".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+    # 检查保存权重文件夹是否存在，不存在则创建
+    if not os.path.exists("save_weights"):
+        os.makedirs("save_weights")
+
+    data_transform = {
+        "train": transforms.Compose([transforms.ToTensor(),
+                                     transforms.RandomHorizontalFlip(0.5)]),
+        "val": transforms.Compose([transforms.ToTensor()])
+    }
+
+    COCO_root = "/data/coco2017"
+
+    # load train data set
+    # coco2017 -> annotations -> instances_train2017.json
+    train_data_set = CocoDetection(COCO_root, "train", data_transform["train"])
+    # 注意这里的collate_fn是自定义的，因为读取的数据包括image和targets，不能直接使用默认的方法合成batch
+    batch_size = 8
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using %g dataloader workers' % nw)
+    train_data_loader = torch.utils.data.DataLoader(train_data_set,
+                                                    batch_size=batch_size,
+                                                    shuffle=True,
+                                                    pin_memory=True,
+                                                    num_workers=nw,
+                                                    collate_fn=train_data_set.collate_fn)
+
+    # load validation data set
+    # coco2017 -> annotations -> instances_val2017.json
+    val_data_set = CocoDetection(COCO_root, "val", data_transform["val"])
+    val_data_set_loader = torch.utils.data.DataLoader(val_data_set,
+                                                      batch_size=batch_size,
+                                                      shuffle=False,
+                                                      pin_memory=True,
+                                                      num_workers=nw,
+                                                      collate_fn=train_data_set.collate_fn)
+
+    # create model num_classes equal background + 80 classes
+    model = create_model(num_classes=81)
+    # print(model)
+
+    model.to(device)
+
+    train_loss = []
+    learning_rate = []
+    val_map = []
+
+    # define optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.001,
+                                momentum=0.9, weight_decay=1e-4)
+    # learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[16, 22], gamma=0.1)
+
+    num_epochs = 26
+    for epoch in range(num_epochs):
+        # train for one epoch, printing every 50 iterations
+        mean_loss, lr = utils.train_one_epoch(model, optimizer, train_data_loader,
+                                              device, epoch, print_freq=50, warmup=True)
+        train_loss.append(mean_loss.item())
+        learning_rate.append(lr)
+
+        # update the learning rate
+        lr_scheduler.step()
+
+        # evaluate on the test dataset
+        coco_info = utils.evaluate(model, val_data_set_loader, device=device)
+
+        # write into txt
+        with open(results_file, "a") as f:
+            # 写入的数据包括coco指标还有loss和learning rate
+            result_info = [str(round(i, 4)) for i in coco_info + [mean_loss.item(), lr]]
+            txt = "epoch:{} {}".format(epoch, '  '.join(result_info))
+            f.write(txt + "\n")
+
+        val_map.append(coco_info[1])  # pascal mAP
+
+        # save weights
+        # 仅保存最后5个epoch的权重
+        if epoch in range(num_epochs)[-5:]:
+            save_files = {
+                'model': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'lr_scheduler': lr_scheduler.state_dict(),
+                'epoch': epoch}
+            torch.save(save_files, "./save_weights/mobile-model-{}.pth".format(epoch))
+
+    # plot loss and lr curve
+    if len(train_loss) != 0 and len(learning_rate) != 0:
+        from plot_curve import plot_loss_and_lr
+        plot_loss_and_lr(train_loss, learning_rate)
+
+    # plot mAP curve
+    if len(val_map) != 0:
+        from plot_curve import plot_map
+        plot_map(val_map)
+
+
+if __name__ == "__main__":
+    main()
