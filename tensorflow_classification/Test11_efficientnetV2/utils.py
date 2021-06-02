482a7d2cb3c6ce633369ed8e7fc40a432c10449e
wz <180662@gree.com.cn>
2021-06-02 09:33:33 +0800

    default closing cache data

--- tensorflow_classification/Test11_efficientnetV2/utils.py
+++ tensorflow_classification/Test11_efficientnetV2/utils.py
@@ -78,7 +78,8 @@ def generate_ds(data_root: str,
                 val_im_height: int = None,
                 val_im_width: int = None,
                 batch_size: int = 8,
-                val_rate: float = 0.1):
+                val_rate: float = 0.1,
+                cache_data: bool = False):
     """
     读取划分数据集，并生成训练集和验证集的迭代器
     :param data_root: 数据根目录
@@ -88,6 +89,7 @@ def generate_ds(data_root: str,
     :param val_im_width:  验证输入网络图像的宽度
     :param batch_size: 训练使用的batch size
     :param val_rate:  将数据按给定比例划分到验证集
+    :param cache_data: 是否缓存数据
     :return:
     """
     assert train_im_height is not None
@@ -120,8 +122,10 @@ def generate_ds(data_root: str,
     # Configure dataset for performance
     def configure_for_performance(ds,
                                   shuffle_size: int,
-                                  shuffle: bool = False):
-        ds = ds.cache()  # 读取数据后缓存至内存
+                                  shuffle: bool = False,
+                                  cache: bool = False):
+        if cache:
+            ds = ds.cache()  # 读取数据后缓存至内存
         if shuffle:
             ds = ds.shuffle(buffer_size=shuffle_size)  # 打乱数据顺序
         ds = ds.batch(batch_size)                      # 指定batch size
@@ -134,13 +138,13 @@ def generate_ds(data_root: str,
 
     # Use Dataset.map to create a dataset of image, label pairs
     train_ds = train_ds.map(process_train_info, num_parallel_calls=AUTOTUNE)
-    train_ds = configure_for_performance(train_ds, total_train, shuffle=True)
+    train_ds = configure_for_performance(train_ds, total_train, shuffle=True, cache=cache_data)
 
     val_ds = tf.data.Dataset.from_tensor_slices((tf.constant(val_img_path),
                                                  tf.constant(val_img_label)))
     total_val = len(val_img_path)
     # Use Dataset.map to create a dataset of image, label pairs
     val_ds = val_ds.map(process_val_info, num_parallel_calls=AUTOTUNE)
-    val_ds = configure_for_performance(val_ds, total_val)
+    val_ds = configure_for_performance(val_ds, total_val, cache=False)
 
     return train_ds, val_ds
