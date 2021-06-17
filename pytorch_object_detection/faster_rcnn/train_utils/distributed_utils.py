0ed02b784a7e2289ac635c3e8a74d50d4c217de8
wz <180662@gree.com.cn>
2021-06-17 14:17:13 +0800

    update distributed_utils for support torch1.9

--- pytorch_object_detection/faster_rcnn/train_utils/distributed_utils.py
+++ pytorch_object_detection/faster_rcnn/train_utils/distributed_utils.py
@@ -318,6 +318,6 @@ def init_distributed_mode(args):
         args.rank, args.dist_url), flush=True)
     torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
                                          world_size=args.world_size, rank=args.rank)
-    torch.distributed.barrier()
+    torch.distributed.barrier(device_ids=[args.rank])
     setup_for_distributed(args.rank == 0)
 
