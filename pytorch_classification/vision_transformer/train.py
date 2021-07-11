ac9c33325a7d34874bd7a22a0fdd95e42334f4db
wz <605169423@qq.com>
2021-07-11 14:09:26 +0800

    update var name

--- pytorch_classification/vision_transformer/train.py
+++ pytorch_classification/vision_transformer/train.py
@@ -35,26 +35,26 @@ def main(args):
                                    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])}
 
     # 实例化训练数据集
-    train_data_set = MyDataSet(images_path=train_images_path,
-                               images_class=train_images_label,
-                               transform=data_transform["train"])
+    train_dataset = MyDataSet(images_path=train_images_path,
+                              images_class=train_images_label,
+                              transform=data_transform["train"])
 
     # 实例化验证数据集
-    val_data_set = MyDataSet(images_path=val_images_path,
-                             images_class=val_images_label,
-                             transform=data_transform["val"])
+    val_dataset = MyDataSet(images_path=val_images_path,
+                            images_class=val_images_label,
+                            transform=data_transform["val"])
 
     batch_size = args.batch_size
     nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
     print('Using {} dataloader workers every process'.format(nw))
-    train_loader = torch.utils.data.DataLoader(train_data_set,
+    train_loader = torch.utils.data.DataLoader(train_dataset,
                                                batch_size=batch_size,
                                                shuffle=True,
                                                pin_memory=True,
                                                num_workers=nw,
                                                collate_fn=train_data_set.collate_fn)
 
-    val_loader = torch.utils.data.DataLoader(val_data_set,
+    val_loader = torch.utils.data.DataLoader(val_dataset,
                                              batch_size=batch_size,
                                              shuffle=False,
                                              pin_memory=True,
