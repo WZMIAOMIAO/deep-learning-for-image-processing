578eff1dd7b589ebf586871f2f0efad2f963c667
wz <180662@gree.com.cn>
2021-12-16 20:48:24 +0800

    update model

--- pytorch_classification/vision_transformer/vit_model.py
+++ pytorch_classification/vision_transformer/vit_model.py
@@ -287,6 +287,23 @@ def _init_vit_weights(m):
         nn.init.ones_(m.weight)
 
 
+def vit_base_patch16_224(num_classes: int = 1000):
+    """
+    ViT-Base model (ViT-B/16) from original paper (https://arxiv.org/abs/2010.11929).
+    ImageNet-1k weights @ 224x224, source https://github.com/google-research/vision_transformer.
+    weights ported from official Google JAX impl:
+    链接: https://pan.baidu.com/s/1zqb08naP0RPqqfSXfkB2EA  密码: eu9f
+    """
+    model = VisionTransformer(img_size=224,
+                              patch_size=16,
+                              embed_dim=768,
+                              depth=12,
+                              num_heads=12,
+                              representation_size=None,
+                              num_classes=num_classes)
+    return model
+
+
 def vit_base_patch16_224_in21k(num_classes: int = 21843, has_logits: bool = True):
     """
     ViT-Base model (ViT-B/16) from original paper (https://arxiv.org/abs/2010.11929).
@@ -304,6 +321,23 @@ def vit_base_patch16_224_in21k(num_classes: int = 21843, has_logits: bool = True
     return model
 
 
+def vit_base_patch32_224(num_classes: int = 1000):
+    """
+    ViT-Base model (ViT-B/32) from original paper (https://arxiv.org/abs/2010.11929).
+    ImageNet-1k weights @ 224x224, source https://github.com/google-research/vision_transformer.
+    weights ported from official Google JAX impl:
+    链接: https://pan.baidu.com/s/1hCv0U8pQomwAtHBYc4hmZg  密码: s5hl
+    """
+    model = VisionTransformer(img_size=224,
+                              patch_size=32,
+                              embed_dim=768,
+                              depth=12,
+                              num_heads=12,
+                              representation_size=None,
+                              num_classes=num_classes)
+    return model
+
+
 def vit_base_patch32_224_in21k(num_classes: int = 21843, has_logits: bool = True):
     """
     ViT-Base model (ViT-B/32) from original paper (https://arxiv.org/abs/2010.11929).
@@ -317,6 +351,23 @@ def vit_base_patch32_224_in21k(num_classes: int = 21843, has_logits: bool = True
                               depth=12,
                               num_heads=12,
                               representation_size=768 if has_logits else None,
+                              num_classes=num_classes)
+    return model
+
+
+def vit_large_patch16_224(num_classes: int = 1000):
+    """
+    ViT-Large model (ViT-L/16) from original paper (https://arxiv.org/abs/2010.11929).
+    ImageNet-1k weights @ 224x224, source https://github.com/google-research/vision_transformer.
+    weights ported from official Google JAX impl:
+    链接: https://pan.baidu.com/s/1cxBgZJJ6qUWPSBNcE4TdRQ  密码: qqt8
+    """
+    model = VisionTransformer(img_size=224,
+                              patch_size=16,
+                              embed_dim=1024,
+                              depth=24,
+                              num_heads=16,
+                              representation_size=None,
                               num_classes=num_classes)
     return model
 
