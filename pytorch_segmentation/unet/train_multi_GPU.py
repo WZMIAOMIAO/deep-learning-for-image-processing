9687cc0790796f8309ca1dcade4ebc465ded6b82
wz <180662@gree.com.cn>
2022-01-08 19:21:51 +0800

    update code

--- pytorch_segmentation/unet/train_multi_GPU.py
+++ pytorch_segmentation/unet/train_multi_GPU.py
@@ -152,7 +152,7 @@ def main(args):
     for epoch in range(args.start_epoch, args.epochs):
         if args.distributed:
             train_sampler.set_epoch(epoch)
-        mean_loss, lr = train_one_epoch(model, optimizer, train_data_loader, device, epoch,
+        mean_loss, lr = train_one_epoch(model, optimizer, train_data_loader, device, epoch, num_classes,
                                         lr_scheduler=lr_scheduler, print_freq=args.print_freq, scaler=scaler)
 
         confmat, dice = evaluate(model, val_data_loader, device=device, num_classes=num_classes)
