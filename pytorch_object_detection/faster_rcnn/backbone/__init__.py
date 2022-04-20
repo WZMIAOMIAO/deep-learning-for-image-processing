d6145658d259fe46ca21c2e57c5e0cf7348e1136
wz <180662@gree.com.cn>
2022-04-20 12:55:05 +0800

    change structure

--- pytorch_object_detection/faster_rcnn/backbone/__init__.py
+++ pytorch_object_detection/faster_rcnn/backbone/__init__.py
@@ -1,3 +1,4 @@
 from .resnet50_fpn_model import resnet50_fpn_backbone
 from .mobilenetv2_model import MobileNetV2
 from .vgg_model import vgg
+from .feature_pyramid_network import LastLevelMaxPool, BackboneWithFPN
