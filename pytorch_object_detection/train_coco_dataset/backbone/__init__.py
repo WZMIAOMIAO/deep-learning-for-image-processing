ec975720f7562b02c2a2288f2f2566f67a4a4f29
wz <180662@gree.com.cn>
2022-04-20 14:08:33 +0800

    update structure

--- pytorch_object_detection/train_coco_dataset/backbone/__init__.py
+++ pytorch_object_detection/train_coco_dataset/backbone/__init__.py
@@ -2,3 +2,4 @@ from .resnet50_fpn_model import resnet50_fpn_backbone
 from .mobilenetv2_model import MobileNetV2
 from .vgg_model import vgg
 from .resnet import *
+from .feature_pyramid_network import BackboneWithFPN, LastLevelMaxPool
