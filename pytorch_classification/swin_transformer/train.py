a2f10c503e0f44e06a848da8eff378de2e8d8d83
lzc <741406584@qq.com>
2022-06-24 16:02:44 +0800

    update swin

--- pytorch_classification/swin_transformer/train.py
+++ pytorch_classification/swin_transformer/train.py
@@ -117,8 +117,8 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_classes', type=int, default=6)
-    parser.add_argument('--epochs', type=int, default=200)
+    parser.add_argument('--num_classes', type=int, default=7)
+    parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--batch-size', type=int, default=128)
     parser.add_argument('--lr', type=float, default=0.0001)
     parser.add_argument('--lrf', type=float, default=0.01)
