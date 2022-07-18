28ef0183f875283655528780593c8cadf403e2c4
wz <180662@gree.com.cn>
2022-07-18 20:40:20 +0800

    update u2net

---
+++ pytorch_segmentation/u2net/transforms.py
@@ -0,0 +1,53 @@
+import random
+from typing import List, Union
+from torchvision.transforms import functional as F
+
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image, target=None):
+        for t in self.transforms:
+            image, target = t(image, target)
+
+        return image, target
+
+
+class ToTensor(object):
+    def __call__(self, image, target):
+        image = F.to_tensor(image)
+        target["mask"] = F.to_tensor(target["mask"])
+        return image, target
+
+
+class RandomHorizontalFlip(object):
+    def __init__(self, prob):
+        self.flip_prob = prob
+
+    def __call__(self, image, target):
+        if random.random() < self.flip_prob:
+            image = F.hflip(image)
+            target["mask"] = F.hflip(target["mask"])
+        return image, target
+
+
+class Normalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, image, target):
+        image = F.normalize(image, mean=self.mean, std=self.std)
+        return image, target
+
+
+class Resize(object):
+    def __init__(self, size: Union[int, List[int]]):
+        self.size = size  # [h, w]
+
+    def __call__(self, image, target=None):
+        image = F.resize(image, self.size)
+        target["mask"] = F.resize(target["mask"], self.size)
+
+        return image, target
