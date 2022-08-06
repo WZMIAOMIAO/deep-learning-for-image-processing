d16c116417d4477fc1e05c6c5a3e665d9c7bdcb3
wz <605169423@qq.com>
2022-08-06 20:27:31 +0800

    update default bs

--- pytorch_keypoint/HRNet/train.py
+++ pytorch_keypoint/HRNet/train.py
@@ -213,7 +213,7 @@ if __name__ == "__main__":
                         metavar='W', help='weight decay (default: 1e-4)',
                         dest='weight_decay')
     # 训练的batch size
-    parser.add_argument('--batch_size', default=1, type=int, metavar='N',
+    parser.add_argument('--batch_size', default=32, type=int, metavar='N',
                         help='batch size when training.')
     # 是否使用混合精度训练(需要GPU支持混合精度)
     parser.add_argument("--amp", default=False, help="Use torch.cuda.amp for mixed precision training")
