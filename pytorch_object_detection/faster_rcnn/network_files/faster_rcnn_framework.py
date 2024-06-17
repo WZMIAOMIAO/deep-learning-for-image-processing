9c8017c5d35027ffc81f3cc0b459593b3dee8f14
chengjinL <l1315827219@163.com>
2024-06-17 10:48:17 +0800

    add MultiScaleRoIPooling Module

--- pytorch_object_detection/faster_rcnn/network_files/faster_rcnn_framework.py
+++ pytorch_object_detection/faster_rcnn/network_files/faster_rcnn_framework.py
@@ -358,10 +358,10 @@ class FasterRCNN(FasterRCNNBase):
             rpn_pre_nms_top_n, rpn_post_nms_top_n, rpn_nms_thresh,
             score_thresh=rpn_score_thresh)
 
-        #  Multi-scale RoIAlign pooling
+        #  Multi-scale RoIAlign or RoIPooling
         if box_roi_pool is None:
             box_roi_pool = MultiScaleRoIAlign(
-                featmap_names=['0', '1', '2', '3'],  # 在哪些特征层进行roi pooling
+                featmap_names=['0', '1', '2', '3'],  # 在哪些特征层进行roi RoIAlign
                 output_size=[7, 7],
                 sampling_ratio=2)
             # box_roi_pool = MultiScaleRoIPooling(
